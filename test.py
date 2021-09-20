367fd82ec339b9243ed7c4c54614bcb6f428c10a
debnathshoham <debnathshoham@gmail.com>
2021-09-20 23:51:03 +0530

    python test added

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import pytest
 from cython_test_coverage.basic_cython import add_test_cython
-#basic
+from python_test import py_diff
+
 @pytest.mark.parametrize("a,b,sum",[(2,5,7), (3,0,3),(-1,5,4), (-1,-3,-4)])
 def test_add_cython(a,b,sum):
      assert add_test_cython(a,b)==sum
+
+@pytest.mark.parametrize("a,b,diff", [(4,3,1),(3,4,-1)])
+def test_diff_python(a,b,diff):
+     assert py_diff(a,b)==diff
