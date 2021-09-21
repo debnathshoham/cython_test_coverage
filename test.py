03da0ee08c707091f34ea7ccbb1e1cab2cc2021d
debnathshoham <debnathshoham@gmail.com>
2021-09-21 23:21:03 +0530

    removed coveragerc

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ import pytest
 from cython_test_coverage.basic_cython import add_test_cython
 from python_test import py_diff
 
+#test cython
 @pytest.mark.parametrize("a,b,sum",[(2,5,7), (3,0,3),(-1,5,4), (-1,-3,-4)])
 def test_add_cython(a,b,sum):
      assert add_test_cython(a,b)==sum
 
+#test python
 @pytest.mark.parametrize("a,b,diff", [(4,3,1),(3,4,-1)])
 def test_diff_python(a,b,diff):
      assert py_diff(a,b)==diff
